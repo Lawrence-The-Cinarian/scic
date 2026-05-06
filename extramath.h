6f903ccf8e393d9488d83b41ead191b2c1107863
Lawrence The Cinarian <lawrencethecinarian@gmail.com>
2026-05-06 13:04:40 +0100

    Refactor trigonometric functions to remove radians parameter

--- extramath.h
+++ extramath.h
@@ -12,49 +12,49 @@
  */
 
 // (1) For finding Sine, Cosine, Tangent, Inverse Sine, Inverse Cosine, and Inverse Tangent
-float disin(float degrees, float radians) {
-  radians = degrees * (PI/180);
+float disin(float degrees) {
+  float radians = degrees * (PI/180);
   float result = sin(radians);
 return result;
 }
 
-float dicos(float degrees, float radians) {
-  radians = degrees * (PI/180);
+float dicos(float degrees) {
+  float radians = degrees * (PI/180);
   float result = cos(radians);
 return result;
 }
 
-float ditan(float degrees, float radians) {
-  radians = degrees * (PI/180);
+float ditan(float degrees) {
+  float radians = degrees * (PI/180);
   float result = tan(radians);
 return result;
 }
 
-float dinvs(float degrees, float radians) {
-  float degrees = radians * (180/PI);
-  float result = asin(radians);
+float dinvs(float degrees) {
+  float radians = degrees * (PI/180);
+  float result = asin(radians) * (180/PI);
 return result;
 }
 
-float dinvc(float degrees, float radians) {
-  float degrees = radians * (180/PI);
-  float result = acos(radians);
+float dinvc(float degrees) {
+  float radians = degrees * (PI/180);
+  float result = acos(radians) * (180/PI);
 return result;
 }
 
-float dinvt(float degrees, float radians) {
-  degree = radians * (180/PI);
-  float result = atan(degree);
+float dinvt(float degrees) {
+  float radians = degrees * (PI/180);
+  float result = atan(radians) * (180/PI);
 return result;
 }
-
+#endif
 // (2) For finding Angles and Sides of a Triangles
 //     For Sine Rule: a/sin A = b/sin B = c/sin C
 //     For Cosine Rule (a*a) = (b*b) + (c*c) - 2bc cos A
 
 
 
-
+#ifdef
 float sine_to_find_side(float a, float A, float B) {
 A = A * (PI/180);
 B = B * (PI/180);

d917f76fcfafc011a2ce701d40bdaa0c4f4bb508
Lawrence-The-Cinarian <cinarilabs@gmail.com>
2026-05-06 13:24:14 +0100

    Fixed the Trigonometric Functions in the Header File (extramath.h)

--- extramath.h
+++ extramath.h
@@ -1,6 +1,5 @@
 #ifndef EXTRAMATH_H
 #define EXTRAMATH_H
-#define  PI (3.142857143)
 #include <math.h>
 /*
  * disin = Degree in Sine
@@ -13,38 +12,35 @@
 
 // (1) For finding Sine, Cosine, Tangent, Inverse Sine, Inverse Cosine, and Inverse Tangent
 float disin(float degrees) {
-  float radians = degrees * (PI/180);
+  float radians = degrees * (M_PI/180);
   float result = sin(radians);
 return result;
 }
 
 float dicos(float degrees) {
-  float radians = degrees * (PI/180);
+  float radians = degrees * (M_PI/180);
   float result = cos(radians);
 return result;
 }
 
 float ditan(float degrees) {
-  float radians = degrees * (PI/180);
+  float radians = degrees * (M_PI/180);
   float result = tan(radians);
 return result;
 }
 
-float dinvs(float degrees) {
-  float radians = degrees * (PI/180);
-  float result = asin(radians) * (180/PI);
+float dinvs(float decimal_sine_number) {
+  float result = asin(decimal_sine_number) * (180/M_PI);
 return result;
 }
 
-float dinvc(float degrees) {
-  float radians = degrees * (PI/180);
-  float result = acos(radians) * (180/PI);
+float dinvc(float decimal_cosine_number) {
+  float result = acos(decimal_cosine_number) * (180/M_PI);
 return result;
 }
 
-float dinvt(float degrees) {
-  float radians = degrees * (PI/180);
-  float result = atan(radians) * (180/PI);
+float dinvt(float decimal_tangent_number) {
+  float result = atan(decimal_tangent_number) * (180/M_PI);
 return result;
 }
 
@@ -54,8 +50,8 @@ return result;
 
 
 float sine_to_find_side(float a, float A, float B) {
-A = A * (PI/180);
-B = B * (PI/180);
+A = A * (M_PI/180);
+B = B * (M_PI/180);
 return (a * sin(B)) / sin(A);
 }
 
